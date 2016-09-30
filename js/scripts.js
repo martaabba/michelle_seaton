938e22fc85ae0b717696466dfe64543ac4ecd78a
martaabba <danika.stochosky@gmail.com>
2016-09-30 17:03:17 +0000

    gallery function added

--- js/scripts.js
+++ js/scripts.js
@@ -16,7 +16,22 @@ $(document).ready(function(){
 	$.stellar();
 
 	//gallery section
-	
+	 // delegate calls to data-toggle="lightbox"
+                $(document).on('click', '[data-toggle="lightbox"]:not([data-gallery="navigateTo"])', function(event) {
+                    event.preventDefault();
+                    return $(this).ekkoLightbox({
+                        onShown: function() {
+                            if (window.console) {
+                                return console.log('Checking our the events huh?');
+                            }
+                        },
+						onNavigate: function(direction, itemIndex) {
+                            if (window.console) {
+                                return console.log('Navigating '+direction+'. Current item: '+itemIndex);
+                            }
+						}
+                    });
+                });
 
  
 	

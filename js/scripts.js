6d72778b426475b992146ebcbd1d9d9e7725a3b2
martaabba <danika.stochosky@gmail.com>
2016-10-07 23:02:54 +0000

    quotes, images, lightbox

--- js/scripts.js
+++ js/scripts.js
@@ -11,24 +11,16 @@ $(document).ready(function(){
                                   });
               return false;
                                               }); //close nav bar smooth scroll
+                                              
+                                              
+  //modal button
+ 
 
 	//Stellar
 	$.stellar();
 
 	//gallery section
-	 // delegate calls to data-toggle="lightbox"
-                $(document).on('click', '[data-toggle="lightbox"]:not([data-gallery="navigateTo"])', function(event) {
-                    event.preventDefault();
-                    return $(this).ekkoLightbox({
-                        onShown: function() {
-                            if (window.console) {
-                                return console.log('Checking out the shows, huh?');
-                            }
-                        },
-				
-						
-                    });
-                });
+	 
 
  
 	
